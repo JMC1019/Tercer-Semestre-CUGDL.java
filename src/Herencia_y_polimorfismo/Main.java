94330bc7184677292a0a9377ce4b520cde334822
Josue Martin del Campo <josuenov3@pm.me>
2026-02-25 08:14:17 -0600

    Herencia y Polimorfismo

--- src/Herencia_y_polimorfismo/Main.java
+++ src/Herencia_y_polimorfismo/Main.java
@@ -2,9 +2,14 @@ package Herencia_y_polimorfismo;
 
 public class Main {
     public static void main(String[] args) {
-        Carro miCarro = new Carro("Toyota", "Corolla", "Rojo", 20000.0, 120.0, "Detenido");
-        miCarro.mostrarInformacion();
-        miCarro.avanzar();
-        miCarro.detenerse();
+        Carro Carro1 = new Carro("BMW", "M5 Competition", "Verde", 3500000.0, 280.0, "");
+        Carro1.mostrarInformacion();
+        Carro1.avanzar();
+        Carro1.detenerse();
+
+        Carro Carro2 = new Carro("Mercedes-Benz", "AMG GT", "Rojo", 4000000.0, 310.0, "");
+        Carro2.mostrarInformacion();
+        Carro2.avanzar();
+        Carro2.detenerse();
     }
 }
